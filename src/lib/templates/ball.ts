cf2b9f1552192aa12ba9a8df93148152a81a9254
mrohmer <rohmer.matze@gmail.com>
2021-08-21 23:45:14 +0200

    fix(template): fix ball template

--- src/lib/templates/ball.ts
+++ src/lib/templates/ball.ts
@@ -5,15 +5,17 @@ export const ballTemplate: StepsGeneratorFn = ({amount}) => {
 	const fmIncreaseSteps = [];
 	const fmDecreaseSteps = [];
 	for (let i = 1; i < amount; i++) {
-		fmIncreaseSteps.push(createStep(`${i} fM, verdoppeln (${(i + 1) * 6})`));
-		fmDecreaseSteps.push(createStep(`${i} fM, Abnahme (${(i + 1) * 6})`));
+		fmIncreaseSteps.push(createStep(`${i} fM, verdoppeln (${(i + 2) * 6})`));
+		fmDecreaseSteps.push(createStep(`${i} fM, Abnahme (${(i + 2) * 6})`));
 	}
 
 	return [
 		createStep(`Magic Ring (6)`),
+		createStep(`verdoppeln (12)`),
 		...fmIncreaseSteps,
 		createStep('Runde', amount + 1),
 		...fmDecreaseSteps.reverse(),
-		createStep(`Abnahme (6)`)
+		createStep(`Abnahme (12)`),
+		createStep(`Vernähen (6)`),
 	]
 }
