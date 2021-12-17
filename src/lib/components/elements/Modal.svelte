f6445b26d93a93cffa47dc50043a11273c9d0759
mrohmer <rohmer.matze@gmail.com>
2021-12-17 03:24:30 +0100

    fix(modal): fix mobile viewport

--- src/lib/components/elements/Modal.svelte
+++ src/lib/components/elements/Modal.svelte
@@ -26,16 +26,29 @@
 
 		&__container {
       position: absolute;
-			top: 100px;
-			left: 100px;
-			right: 100px;
-			max-height: calc(100vh - 200px);
+			top: 50px;
+			left: 50px;
+			right: 50px;
+			max-height: calc(100vh - 100px);
 			border-radius: 10px;
       box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.25);
 			background: white;
 			padding: 20px;
+			width: calc(100vw - 100px);
 			max-width: 600px;
 			margin: auto;
+
+			@media (max-width: 600px) {
+				top: 100px;
+        left: 0;
+        right: 0;
+        max-height: calc(100vh - 100px);
+        height: calc(100vh - 100px);
+        width: 100vw;
+        max-width: 100vw;
+				border-bottom-left-radius: 0;
+				border-bottom-right-radius: 0;
+			}
 		}
   }
 </style>
