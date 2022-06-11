886560e04ac588444e59d57267067e0a53612d1f
mrohmer <rohmer.matze@gmail.com>
2022-06-11 18:23:39 +0200

    fix: make nav bar static

--- src/routes/projects/[projectId]/__layout.svelte
+++ src/routes/projects/[projectId]/__layout.svelte
@@ -32,11 +32,11 @@
 {#if loading}
 	loading...
 {:else if $project?.name }
-	<nav class="flex py-2">
-		<div class="align-middle py-0.5 px-2 -ml-2 mt-px border-b-2 border-transparent">
+	<nav class="flex py-2 fixed bg-white dark:bg-zinc-900 z-50 inset-x-0 top-0 w-screen">
+		<div class="align-middle py-0.5 px-2 border-b-2 border-transparent">
 			<a href="/"><Icon icon={homeIcon}/></a>
 		</div>
-		<div class="inline-block align-middle py-1 px-2 border-b-2 border-transparent font-light"
+		<div class="inline-block align-middle py-1 px-2 border-b-2 border-transparent font-light max-w-[75px] truncate sm:max-w-xs md:max-w-md"
 				 class:border-secondary={$page.path === `/projects/${$project.id}`}>
 			<a href="/projects/{$project.id}">{$project.name}</a>
 		</div>
@@ -50,14 +50,14 @@
 				{/each}
 			{/if}
 		</div>
-		<div class="align-middle py-0.5 px-2 -mr-2 mt-px border-b-2 border-transparent"
+		<div class="align-middle py-0.5 px-2 mt-px border-b-2 border-transparent"
 				 class:border-secondary={$page.path === `/projects/${$project.id}/parts/add`}>
 			<a href="/projects/{$project.id}/parts/add">
 				<Icon icon={addIcon}/>
 			</a>
 		</div>
 	</nav>
-	<div class="pt-4">
+	<div class="pt-14">
 		<slot />
 	</div>
 {:else}
